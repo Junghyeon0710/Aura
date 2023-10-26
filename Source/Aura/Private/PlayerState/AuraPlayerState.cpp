75c95a08cc4b34d9321756e8993646580cfa9139
Junghyeon <sie08357@naver.com>
2023-10-26 16:54:48 +0900

    플레이어 스테이트 추가 어빌리티시스템 컴포넌트 추가 애트뷰셋 추가

---
+++ Source/Aura/Private/PlayerState/AuraPlayerState.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerState/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100.f; //서버가 클라이언트 업데이트를 시도하는 빈도
+
+}
