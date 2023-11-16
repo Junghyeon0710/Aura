3ef919b8c90cc928c05ec2114b397cf31e865594
Junghyeon <sie08357@naver.com>
2023-11-16 14:39:35 +0900

    Environment Queries사영 EQS쿼리 사용해서 최적의 거리 얻음

---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraMeleeAbttack.h
@@ -0,0 +1,17 @@
+// Copyright Druid Mechanics
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraDamageGameplayAbility.h"
+#include "AuraMeleeAbttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraMeleeAbttack : public UAuraDamageGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
