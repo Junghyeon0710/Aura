4abffa873c215789dff3df7554eca78a709bf187
Junghyeon <sie08357@naver.com>
2023-11-27 16:53:04 +0900

    파이어볼 spread 구하기

--- Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
@@ -2,6 +2,7 @@
 
 
 #include "AbilitySystem/Abilities/AuraFireBolt.h"
+#include "Kismet/GameplayStatics.h"
 
 
 FString UAuraFireBolt::GetDescription(int32 Level)
@@ -97,4 +98,51 @@ FString UAuraFireBolt::GetNextLevelDescription(int32 Level)
 		Cooldown,
 		FMath::Min(Level, NumProjectiles),
 		ScaledDamage);
+}
+
+void UAuraFireBolt::SpawnProjectiles(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag, bool bOverridePitch, float PitchOverride, AActor* HomingTarget)
+{
+	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
+	if (!bIsServer) return;
+
+	const FVector SocketLocation = ICombatInterface::Execute_GetCombatSocketLocation(
+		GetAvatarActorFromActorInfo(),
+		SocketTag);
+	FRotator Rotation = (ProjectileTargetLocation - SocketLocation).Rotation();
+	//(A-B).Roation 는 B에서 A의 방향을 알려준다.
+	
+	if (bOverridePitch)
+	{
+		Rotation.Pitch = PitchOverride;
+	}
+
+	const FVector Forward = Rotation.Vector();
+	const FVector LeftOfSpread = Forward.RotateAngleAxis(-ProjectileSpread / 2.f, FVector::UpVector);
+	const FVector RightOfSpread = Forward.RotateAngleAxis(ProjectileSpread / 2.f, FVector::UpVector);
+
+	//NumProjectiles = FMath::Min(MaxnumProjectile, GetAbilityLevel());
+	if (NumProjectiles > 1)
+	{
+		const float DeltaSpread = ProjectileSpread / (NumProjectiles - 1);
+		for (int32 i = 0; i < NumProjectiles; i++)
+		{		
+			//적의 45도 왼쪽 각도에서 Z축으로 DeltaSpread *i만큼 회전
+			//0도 부터 처음 SpawnSpread값까지 구함
+			const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
+			const FVector Start = SocketLocation;
+			UKismetSystemLibrary::DrawDebugArrow(
+				GetAvatarActorFromActorInfo(),
+				Start,
+				Start + Direction * 75.f,
+				1,
+				FLinearColor::Red,
+				120,
+				1);
+		}
+	}
+	else
+	{
+
+	}
+	
 }
